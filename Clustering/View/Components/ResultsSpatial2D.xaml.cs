6f7627fb8cfa8c2d9b27a226ae0e3013241ad7d0
Péter Bálint <peter.balint18@gmail.com>
2026-09-23 01:23:31 +0200

    Singular canvas export, as is

--- Clustering/View/Components/ResultsSpatial2D.xaml.cs
+++ Clustering/View/Components/ResultsSpatial2D.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.Win32;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Clustering.View.Components
@@ -12,9 +13,36 @@ namespace Clustering.View.Components
     /// </summary>
     public partial class ResultsSpatial2D : UserControl
     {
+        private Canvas canvas;
         public ResultsSpatial2D()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            canvas = FindVisualChild<Canvas>(PointsItemsControl);
+        }
+
+        private static childItem FindVisualChild<childItem>(DependencyObject obj)
+        where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                {
+                    return (childItem)child;
+                }
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
         }
 
         private void SaveAsImageButton_Click(object? sender, RoutedEventArgs e)
@@ -28,21 +56,19 @@ namespace Clustering.View.Components
             };
             if (dialog.ShowDialog() == true)
             {
-                /*Canvas canvas = (Canvas)PointsItemsControl.ItemsPanel;
-                Rect rect = new Rect(MyCanvas.RenderSize);
+                canvas.Background = Brushes.AliceBlue;
+
+                Rect rect = new Rect(canvas.RenderSize);
                 RenderTargetBitmap rtb = new RenderTargetBitmap((int)rect.Right,
                   (int)rect.Bottom, 96d, 96d, System.Windows.Media.PixelFormats.Default);
-                rtb.Render(MyCanvas);
-                //endcode as PNG
+                rtb.Render(canvas);
+
                 BitmapEncoder encoder = new PngBitmapEncoder();
                 encoder.Frames.Add(BitmapFrame.Create(rtb));
 
                 using var stream = File.Create(dialog.FileName);
-                encoder.Save(stream);*/
+                encoder.Save(stream);
             }
-
-
-
         }
     }
 }
